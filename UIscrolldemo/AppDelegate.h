04acca65748ca63eb3d3c73068470ea8f7df841c
Add <Add@AddtekiMac-mini.local>
2013-07-15 09:57:32 +0800

    Initial Commit

---
+++ UIscrolldemo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  UIscrolldemo
+//
+//  Created by Add on 13-7-15.
+//  Copyright (c) 2013年 shinyv. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ UIscrolldemo/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  UIscrolldemo
+//
+//  Created by Add on 13-7-15.
+//  Copyright (c) 2013年 shinyv. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
